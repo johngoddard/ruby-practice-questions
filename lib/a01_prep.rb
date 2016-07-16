57e5a1ae0d665b0b792b8a2305def1f047078594
John Goddard <johngoddard243@gmail.com>
2016-07-16 14:38:52 -0700

    specs added

---
+++ lib/a01_prep.rb
@@ -0,0 +1,44 @@
+require 'byebug'
+class Array
+
+  def my_each(&prc)
+  end
+
+  def my_each_with_index(&prc)
+  end
+
+  def my_select(&prc)
+  end
+
+  def my_reject(&prc)
+  end
+
+  def my_any?(&prc)
+  end
+
+  def my_all?(&prc)
+  end
+
+  def my_flatten
+  end
+
+  def my_zip(*arrs)
+  end
+
+  def my_rotate(num=1)
+
+  end
+
+  def my_join(str = "")
+  end
+
+  def my_reverse
+  end
+
+  def my_quick_sort(&prc)
+  end
+
+  def my_bsearch(target, &prc)
+  end
+
+end
