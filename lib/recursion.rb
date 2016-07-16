a2afddedd1f5a26e67291c677367d99c9582cbcf
John Goddard <johngoddard243@gmail.com>
2016-07-16 15:50:56 -0700

    spacing

--- lib/recursion.rb
+++ lib/recursion.rb
@@ -15,9 +15,10 @@ class RecursionMachine
 
   end
 
-  # return b^n recursively. Your solution should accept
-  # negative values for n
+  # return b^n recursively. Your solution should accept negative values
+  # for n
   def exponent(b, n)
 
   end
+
 end
