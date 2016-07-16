73d781911a416128997547cbc7ee29d902ac3fd2
John Goddard <johngoddard243@gmail.com>
2016-07-16 16:08:40 -0700

    added spec case numbers

--- spec/recursion_spec.rb
+++ spec/recursion_spec.rb
@@ -55,15 +55,15 @@ describe "exponent" do
 end
 
 describe "Make better change" do
-  it "Returns the smallest possible array of coins" do
+  it "Returns the smallest possible array of coins: case 1" do
     expect(r.make_better_change(24, [10,7,1])).to eq([10,7,7])
   end
 
-  it "Returns the smallest possible array of coins" do
+  it "Returns the smallest possible array of coins: case 2" do
     expect(r.make_better_change(25, [10,7,1])).to eq([10,7,7,1])
   end
 
-  it "Returns the smallest possible array of coins" do
+  it "Returns the smallest possible array of coins: case 3" do
     expect(r.make_better_change(25, [10,8,7,1])).to eq([10,8,7])
   end
 end
