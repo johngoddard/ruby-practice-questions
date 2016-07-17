10c15a1b6ed8020c6b53da368789caaf1d91eb36
John Goddard <johngoddard243@gmail.com>
2016-07-17 15:42:00 -0700

    recursive number base changer

--- lib/recursion.rb
+++ lib/recursion.rb
@@ -62,6 +62,16 @@ class RecursionMachine
 
   end
 
+  # Write a recursive method that takes in a base 10 number n and
+  # converts it to a base b number. Return the new number as a string
+  #
+  # E.g. base_converter(5, 2) == "101"
+  # base_converter(31, 16) == "1f"
+
+  def base_converter(num, b)
+
+  end
+
   # CHALLENGE: Eight queens puzzle precursor
   #
   # Write a recursive method that generates all 8! possible unique ways to
--- solutions/recursion.rb
+++ solutions/recursion.rb
@@ -97,6 +97,13 @@ class RecursionMachine
     digit_sum > 10 ? digital_root(digit_sum) : digit_sum
   end
 
+  def base_converter(num, b)
+    return "" if num == 0
+
+    digits = %w(0 1 2 3 4 5 6 7 8 9 a b c d e f)
+    base_converter(num/b, b) + digits[num % b]
+  end
+
   def eight_queens_possibilities(row_num, taken_columns, positions)
     positions ||= []
     return [positions] if row_num == 8
